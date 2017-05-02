81396fc159a2499c9dd9db97afdbcc25adc96f33
pisanggoreng <7ofpentacles@gmail.com>
2017-05-02 21:37:30 +0700

    first init

---
+++ js/map.js
@@ -0,0 +1,228 @@
+function myMap () {
+  let myCenter = new google.maps.LatLng(-6.2598172, 106.7682235,17)
+  let mapProp = {
+    center: myCenter,
+    zoom: 16,
+    scrollwheel: false,
+    draggable: false,
+    mapTypeId: google.maps.MapTypeId.TERRAIN,
+    styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#523735"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#c9b2a6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#dcd2be"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#ae9e90"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#93817c"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#a5b076"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#447530"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fdfcf8"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f8c967"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#e9bc62"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e98d58"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#db8555"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#806b63"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8f7d77"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#b9d3c2"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#92998d"
+      }
+    ]
+  }
+]
+  }
+  let map = new google.maps.Map(document.getElementById('googleMap'), mapProp)
+  let marker = new google.maps.Marker({position: myCenter})
+  marker.setMap(map)
+}
---
+++ js/surgical-regular.js
@@ -0,0 +1,349 @@
+document.write(`
+  <div id="surgicalRegular" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Surgical Regular (Lightly Powdered)</h4>
+              </div>
+              <div class="modal-body">
+                  <p>Product Benefit</p>
+                  <ul>
+                      <li>The transparency of Premium Protos will help the user to reassure that there is no leakage / penetration of blood or body fluids between the gloves and the hands of user.</li>
+                      <li>These gloves are more elastic than other product.</li>
+                      <li>Puncture of the gloves is more resistant than other product.</li>
+                      <li>These gloves give more flexibility to the hand than other product.</li>
+                      <li>It has high sensibility than other product.</li>
+                      <li>It gives less fatigue to the user's hand.</li>
+                      <li>The texture of the palm enhance the high grip between the hands / gloves & surgical devices / tools used by surgeons. (The high grip will help the surgeon to hold the surgical tool intact in their hand without any slippage).</li>
+                      <li>Due to the high grip, sensibility, flexibility and fatigue resistance, this product can be used for major and minor surgery as well as cosmetic surgery.</li>
+                  </ul>
+                  <p>Products Numbers</p>
+                  <table class="table table-bordered">
+                      <thead>
+                          <tr>
+                              <th>Item</th>
+                              <th>Size</th>
+                              <th>Length</th>
+                              <th>Inches</th>
+                              <th>Palm Width</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>PRO-71060</td>
+                              <td>6</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>76 +/- 6 mm</td>
+                          </tr>
+                          <tr>
+                              <td>PRO-71065</td>
+                              <td>6.5</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>83 +/- 6 mm</td>
+                          </tr>
+                          <tr>
+                              <td>PRO-71070</td>
+                              <td>7</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>89 +/- 6 mm</td>
+                          </tr>
+                          <tr>
+                              <td>PRO-71075</td>
+                              <td>7.5</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>95 +/- 6 mm</td>
+                          </tr>
+                          <tr>
+                              <td>PRO-71080</td>
+                              <td>8</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>102 +/- 6 mm</td>
+                          </tr>
+                          <tr>
+                              <td>PRO-71085</td>
+                              <td>8.5</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>108 +/- 6 mm</td>
+                          </tr>
+                          <tr>
+                              <td>PRO-71090</td>
+                              <td>9</td>
+                              <td>295 ± 5 mm</td>
+                              <td>10.5</td>
+                              <td>114 +/- 6 mm</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Mill Thickness</p>
+                  <table class="table table-bordered">
+                      <thead>
+                          <tr>
+                              <th>Item</th>
+                              <th>Finger</th>
+                              <th>Palm</th>
+                              <th>Cuff</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>All Sizes</td>
+                              <td>5.0 mill</td>
+                              <td>4.0 mill</td>
+                              <td>4.0 mill</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Tensile Properties</p>
+                  <table class="table table-bordered">
+                      <thead>
+                          <tr>
+                              <th>Item</th>
+                              <th>Before Aging</th>
+                              <th>After Aging</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>Tensile Strenght Mpa (Minimum)</td>
+                              <td>24</td>
+                              <td>18</td>
+                          </tr>
+                          <tr>
+                              <td>Ultimate Elogation % Minimum</td>
+                              <td>750</td>
+                              <td>560</td>
+                          </tr>
+                          <tr>
+                              <td>Strees at 500 % elongation (Max)</td>
+                              <td>5.5 Mpa</td>
+                              <td> </td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Performance Requirements - AQL</p>
+                  <table class="table table-bordered">
+                      <thead>
+                          <tr>
+                              <th rowspan="2">Item</th>
+                              <th rowspan="2">Related Defects</th>
+                              <th colspan="2">Inspection</th>
+                          </tr>
+                          <tr>
+                              <th>height</th>
+                              <th>weight</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>Sterility</td>
+                              <td>Fails Sterility</td>
+                              <td></td>
+                              <td>N/A</td>
+                          </tr>
+                          <tr>
+                              <td>Watertightness</td>
+                              <td>Holes</td>
+                              <td>G-1</td>
+                              <td>1.5</td>
+                          </tr>
+                          <tr>
+                              <td>Dimentions</td>
+                              <td>Width, Lenght, Thickness</td>
+                              <td>S-2</td>
+                              <td>4.0</td>
+                          </tr>
+                          <tr>
+                              <td>Physycal Properties</td>
+                              <td>Before and After Aging</td>
+                              <td>S-2</td>
+                              <td>4.0</td>
+                          </tr>
+                          <tr>
+                              <td>Visual Defect</td>
+                              <td>Stains,etc</td>
+                              <td>S-4</td>
+                              <td>2.5</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Specification</p>
+                  <table class="table table-bordered">
+                      <tbody>
+                          <tr>
+                              <td>Sterility</td>
+                              <td>:</td>
+                              <td>Sterile</td>
+                          </tr>
+                          <tr>
+                              <td>Intended Use</td>
+                              <td>:</td>
+                              <td>Medical Activity Including Surgery</td>
+                          </tr>
+                          <tr>
+                              <td>Basic Material</td>
+                              <td>:</td>
+                              <td>Natural Rubber Latex</td>
+                          </tr>
+                          <tr>
+                              <td>Class Product</td>
+                              <td>:</td>
+                              <td>Class Ila, Medical Device</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Material Safety Data Identification</p>
+                  <table class="table table-bordered">
+                      <tbody>
+                          <tr>
+                              <td>Product Name</td>
+                              <td>:</td>
+                              <td>Protos</td>
+                          </tr>
+                          <tr>
+                              <td>Product</td>
+                              <td>:</td>
+                              <td>Latex Surgical Right and Left Gloves. Lightly Powdered</td>
+                          </tr>
+                          <tr>
+                              <td>Case #</td>
+                              <td>:</td>
+                              <td>Not Established</td>
+                          </tr>
+                          <tr>
+                              <td>IATO/IACO</td>
+                              <td>:</td>
+                              <td>Not Restricted</td>
+                          </tr>
+                          <tr>
+                              <td>Hazard Rating</td>
+                              <td>:</td>
+                              <td>Not Restricted</td>
+                          </tr>
+                          <tr>
+                              <td>D.O.T Rating</td>
+                              <td>:</td>
+                              <td>Not Restricted</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Physical Data</p>
+                  <table class="table table-bordered">
+                      <tbody>
+                          <tr>
+                              <td>Natural Solid Rubber</td>
+                              <td>:</td>
+                              <td>-95.8%</td>
+                          </tr>
+                          <tr>
+                              <td>Sulfur</td>
+                              <td>:</td>
+                              <td>-1.0 %</td>
+                          </tr>
+                          <tr>
+                              <td>Zinc Accelerators</td>
+                              <td>:</td>
+                              <td>-1.2 %</td>
+                          </tr>
+                          <tr>
+                              <td>Others</td>
+                              <td>:</td>
+                              <td>-2%</td>
+                          </tr>
+                          <tr>
+                              <td>Sterilization Methode</td>
+                              <td>:</td>
+                              <td>ETO gas</td>
+                          </tr>
+                          <tr>
+                              <td>Orientation</td>
+                              <td>:</td>
+                              <td>Anatomic</td>
+                          </tr>
+                          <tr>
+                              <td>Donning Powder</td>
+                              <td>:</td>
+                              <td>USP Grade Modified Cornstarch Powder</td>
+                          </tr>
+                          <tr>
+                              <td>Powder</td>
+                              <td>:</td>
+                              <td>Powdered Surgical Sterile</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Fire And Explosion</p>
+                  <table class="table table-bordered">
+                      <tbody>
+                          <tr>
+                              <td>Stability</td>
+                              <td>:</td>
+                              <td>Stable</td>
+                          </tr>
+                          <tr>
+                              <td>Flash Point</td>
+                              <td>:</td>
+                              <td>N/A</td>
+                          </tr>
+                          <tr>
+                              <td>Flash Point</td>
+                              <td>:</td>
+                              <td>Foam, Carbon Dioxide, Water</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+                  <p>Normal Precautions</p>
+                  <table class="table table-bordered">
+                      <tbody>
+                          <tr>
+                              <td>Ventilation</td>
+                              <td>:</td>
+                              <td>Not necessary under normal conditions</td>
+                          </tr>
+                          <tr>
+                              <td>Equipment</td>
+                              <td>:</td>
+                              <td>Not necessary</td>
+                          </tr>
+                          <tr>
+                              <td>Spill/Clean up</td>
+                              <td>:</td>
+                              <td>N/A</td>
+                          </tr>
+                          <tr>
+                              <td>Eyes</td>
+                              <td>:</td>
+                              <td>N/A</td>
+                          </tr>
+                          <tr>
+                              <td>Storage</td>
+                              <td>:</td>
+                              <td>No special precautions necessary. Store in a cool dry place at room temperature</td>
+                          </tr>
+                      </tbody>
+                  </table>
+
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+          </div>
+      </div>
+  </div>
+`)
